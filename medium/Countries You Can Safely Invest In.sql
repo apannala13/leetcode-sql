9d25d22a43c8d006fa6109dc3d623e91f33b169e
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-06-22 21:54:25 -0700

    Update Countries You Can Safely Invest In.sql

--- medium/Countries You Can Safely Invest In.sql
+++ medium/Countries You Can Safely Invest In.sql
@@ -2,31 +2,36 @@
 --get the total average call duration 
 --filter for countries with > call duration than total
 
-
 with t1 as(
-    select 
+    select
         caller_id as id, 
         duration
-    from Calls 
-    union all
-    select callee_id as id, 
-    duration
+    from calls
+    union all 
+    select 
+        callee_id as id, 
+        duration
     from calls
 ), 
 t2 as(
     select 
-        c.name as country, 
-        avg(t1.duration) as country_duration
-    from country c 
-    join Person p 
-    on substring(p.phone_number, 1, 3) = country_code
-    join t1 on t1.id = p.id 
-    group by 1
+        avg(duration) as global_duration
+    from t1
 ), 
 t3 as(
     select 
-        avg(duration) as global_duration
-    from t1
+        c.name as country, 
+        avg(duration) as country_duration
+    from 
+        person p 
+    join country c 
+    on substring(p.phone_number, 1, 3) = c.country_code 
+    join t1 
+        on p.id = t1.id
+    group by 1
 )
-select country from t2 where country_duration > (select global_duration from t3)
 
+select 
+    country
+from t3 
+where country_duration > (select global_duration from t2)
