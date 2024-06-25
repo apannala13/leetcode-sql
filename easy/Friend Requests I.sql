9d6fb5249c9940e60e29ee3d6f3095b5b2f7a598
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-06-24 21:25:42 -0700

    Update Friend Requests I.sql

--- easy/Friend Requests I.sql
+++ easy/Friend Requests I.sql
@@ -1,14 +1,16 @@
 with t1 as(
     select 
-        count(distinct(concat(sender_id, send_to_id))) as nr 
+        count(distinct(concat(sender_id, send_to_id))) as senders
     from FriendRequest
 ), 
 t2 as(
     select 
-        count(distinct(concat(requester_id, accepter_id))) as na 
+        count(distinct(concat(requester_id, accepter_id))) 
+            as accepters
     from RequestAccepted
 )
 select 
-    case when nr = 0 then 0.0 
-    else coalesce(round(na/nr::decimal, 2), 0) end as accept_rate
-from t1, t2
+    case when senders = 0 then 0.0 else
+    coalesce(round(accepters/senders::decimal, 2), 0) end as accept_rate
+from t1, t2 
+
